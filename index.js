18d061b7253f4a5b5c2294a6cd8fcaf62fceeefa
ichunks <readclient@gmail.com>
2023-05-12 09:58:09 +0800

    date

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ export class Plugin {
   get config () {
     return this._config
   }
+  // 销毁实例
   destory () {
-   
+    // 销毁实例
+    // 获取当前日期
+    let date = new Date()
+    let year = date.getFullYear()
+    let month = date.getMonth() + 1
   }
 }
